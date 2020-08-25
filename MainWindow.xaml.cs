b91cd4e6f0a0c74a14e35f273d9ff3e4a2506ae0
Torchok <Torchok19081986@users.noreply.github.com>
2020-08-25 09:06:00 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MaterialDesignThemes.Wpf.Transitions;
+
+namespace TileWithContextmenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cm_Opened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cm_Closed(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MetroWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void emailtilebtn_Click(object sender, RoutedEventArgs e)
+        { 
+
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
