969fdb71662b8f24f8204e01a8d59d3202048a0a
Tim <tim.uniewski@gmail.com>
2020-08-25 09:53:41 +0200

    Fix

---
+++ ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileWithContextmenu
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        // Let's have a static Instance which we can access from anywhere
+        static ViewModel _Instance;
+        public static ViewModel Instance
+        {
+            get
+            {
+                return _Instance ??= new ViewModel();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string Name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+
+        private int _CurrentIndex = 0;
+        public int CurrentIndex
+        {
+            get { return _CurrentIndex; }
+            set { _CurrentIndex = value; OnPropertyChanged(nameof(CurrentIndex)); }
+        }
+
+    }
+}
